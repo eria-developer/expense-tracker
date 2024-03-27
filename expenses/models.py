c1f16a44dc11b27f41c6ec8c7b6d7f3a69260fa3
Talemwa Eria Jackson <120446528+Talemwa-Eria-Jackson@users.noreply.github.com>
2024-03-27 18:19:42 +0300

    Added the Expense model

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Expense(models.Model):
+    CATEGORY_CHOICES = [
+        ("Food", "Food"),
+        ("Housing", "Housing"),
+        ("Transportation", "Transportation"),
+        ("Health", "Health"),
+        ("Entertainment", "Entertainment"),
+        ("Other", "Other"),
+    ]
+    name = models.CharField(max_length=100, null=False, blank=False)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    quantity = models.DecimalField(decimal_places=2)
+    unit_amount = models.DecimalField(decimal_places=2)
+    date = models.DateField(default=date.today)
+
+    def __str__(self):
+        return f"{self.name.title()} Qnt: {self.quantity} UnitAmount: {self.unit_amount}"
