bb14a5fbe2a5a7c0ceb2a65d6395b018ae290fd6
Talemwa Eria Jackson <120446528+Talemwa-Eria-Jackson@users.noreply.github.com>
2024-03-27 18:39:00 +0300

    added the add_expense view

--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Expense
 
-# Create your views here.
+def add_expense(request):
+    """handles adding new expense"""
+    if request.method == "POST":
+        # getting values from the form 
+        name = request.POST.get("name")
+        category = request.POST.get("category")
+        quantity = request.POST.get("quantity")
+        unit_amount = request.POST.get("unit_amount")
+        date = request.POST.get("date")
+
+        # saving the values into expense variable
+        expense = Expense(name=name, category=category, quantity=quantity, unit_amount=unit_amount, date=date)
+
+        # if form not valid, we render it back with its data preserved
+        if not expense.clean():
+            return render(request, "expenses/add_expense.html", {"form_data": request.POST})
+        
+        # if the form is valid, we save it 
+        expense.save()
+        return redirect()
+    else:
+        return render(request, "expenses/add_expense.html")
