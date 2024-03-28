5a9abc6085f5c3055b7c1a957b26028199280105
Talemwa Eria Jackson <120446528+Talemwa-Eria-Jackson@users.noreply.github.com>
2024-03-28 11:12:06 +0300

    added manual form validation without using django forms

--- expenses/views.py
+++ expenses/views.py
@@ -7,6 +7,8 @@ def home(request):
 
 def add_expense(request):
     """handles adding new expense"""
+    errors = {}
+    category_choices = Expense.CATEGORY_CHOICES
     if request.method == "POST":
         # getting values from the form 
         name = request.POST.get("name")
@@ -15,15 +17,34 @@ def add_expense(request):
         unit_amount = request.POST.get("unit_amount")
         date = request.POST.get("date")
 
-        # saving the values into expense variable
-        expense = Expense(name=name, category=category, quantity=quantity, unit_amount=unit_amount, date=date)
+        # manual validation of the form 
+        if not name:
+            errors["name"] = "Please insert the expense name!"
+        if not category:
+            errors["category"] = "Please insert the expense category!"
+        if not quantity:
+            errors["quantity"] = "Please insert the expense quantity!"
+        if not unit_amount:
+            errors["unit_amount"] = "Please insert the expense unit_amount!"
 
-        # if form not valid, we render it back with its data preserved
-        if not expense.clean():
-            return render(request, "expenses/add_expense.html", {"form_data": request.POST})
-        
-        # if the form is valid, we save it 
-        expense.save()
-        return redirect()
+        if not errors:
+        # saving the values into expense variable
+            expense = Expense(name=name, category=category, quantity=quantity, unit_amount=unit_amount, date=date)
+
+            context = {
+                "form_data": request.POST,
+                "category_choices": category_choices,
+                "errors": errors,
+            }
+            # if form not valid, we render it back with its data preserved
+            if not expense.clean():
+                return render(request, "expenses/add_expense.html", context)
+            
+            # if the form is valid, we save it 
+            expense.save()
+            return redirect()
     else:
-        return render(request, "expenses/add_expense.html")
+        context = {
+            "category_choices": category_choices,
+        }
+        return render(request, "expenses/add_expense.html", context)
